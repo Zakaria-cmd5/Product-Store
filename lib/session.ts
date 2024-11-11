54f955dd59a8209f2a2b8ec7794dcf0687e9ba28
Zakaria Asad <zasad2587@gmail.com>
2024-11-11 13:15:38 +0200

    Update the schema to add a adming user

--- lib/session.ts
+++ lib/session.ts
@@ -9,7 +9,7 @@ export async function createSession(userId: string) {
   const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
   const session = await encrypt({ userId, expiresAt });
 
-  const cookie = await cookies(); // Await the cookies promise
+  const cookie = await cookies(); 
   cookie.set("session", session, {
     httpOnly: true,
     secure: true,
@@ -18,7 +18,7 @@ export async function createSession(userId: string) {
 }
 
 export async function deleteSession() {
-  const cookie = await cookies(); // Await the cookies promise
+  const cookie = await cookies(); 
   cookie.delete("session");
 }
 
--- utils/getUserSession.ts
+++ utils/getUserSession.ts
@@ -4,7 +4,7 @@ import { decrypt } from "@/lib/session";
 import { cookies } from "next/headers";
 
 export const getUserSession = async () => {
-  const allCookies = await cookies(); // Await the promise here
+  const allCookies = await cookies();
   const cookie = allCookies.get("session")?.value;
   const session = cookie ? await decrypt(cookie) : null;
   const sessionId = session?.userId?.toString();
